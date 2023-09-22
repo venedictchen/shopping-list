a4712f4e5dc5f548b6f9d4f58022b753979a6129
venedictchen <venedictchen@gmail.com>
2023-09-22 22:26:52 +0700

    fixing models and forms

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,10 @@
 from django.forms import ModelForm
 from main.models import Product
+from django import forms   
+from django.contrib.auth.models import User   # NEW!
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ["name","price","description"]
+        user = forms.ModelChoiceField(queryset=User.objects.all(), required=True)  # NEW 
